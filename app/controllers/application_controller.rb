3f6851aa0cb8eb21b429c383d47c14d53efe2e38
Dinh Duc Hai <hdtht198@gmail.com>
2019-09-24 22:40:56 +0700

    Chapter_5: Filling the layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
-  protect_from_forgery with: :exception
-
   private
 
   def set_locale
--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,2 +1,2 @@
-import { createConsumer } from "@rails/actioncable"
-export default createConsumer()
+import{createConsumer}
+export default createConsumer
